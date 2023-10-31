0e9cd96000845119adc42905827f8942554325e3
Idowu <jacquelynne.idowu@fiserv.com>
2023-10-31 14:09:58 -0400

    Algo Hour 5

--- Solution.java
+++ Solution.java
@@ -1,7 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public static char mostFrequentChar(String s) {
+        HashMap<Character, Integer> map =new HashMap<>();
+        char answer='0';
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<s.length();i++){
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0)+1);
+        }
+        for(Map.Entry<Character, Integer> entry: map.entrySet()){
+            if(entry.getValue()>max){
+                max=entry.getValue();
+                answer=entry.getKey();
+            }
+        }
+
       
-      return;
+      return answer;
     }
   
     public static void main(String[] args) {
